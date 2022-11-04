4e3b4a797995b1126b4903bf2ebf9c0b0bbd7385
Dmitriy <dimazurenko@gmail.com>
2022-11-04 14:53:59 +0400

    feat: add js function

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,16 +1,17 @@
-/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+/* Check webp support, add webp or no-webp class for HTML */
 export function isWebp() {
-	// Проверка поддержки webp
-	function testWebP(callback) {
-		let webP = new Image();
-		webP.onload = webP.onerror = function () {
-			callback(webP.height == 2);
-		};
-		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-	}
-	// Добавление класса _webp или _no-webp для HTML
-	testWebP(function (support) {
-		let className = support === true ? 'webp' : 'no-webp';
-		document.documentElement.classList.add(className);
-	});
+  // webp support check
+  function testWebP(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  }
+  // Adding the _webp or _no-webp class for HTML
+  testWebP(function (support) {
+    let className = support === true ? 'webp' : 'no-webp';
+    document.documentElement.classList.add(className);
+  });
 }
