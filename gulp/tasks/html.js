3f5a9dedc53644b6b68947b0bb4a7f5992b60644
Dmitriy <dimazurenko@gmail.com>
2022-11-04 14:33:55 +0400

    refactor: change comments

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,35 +3,39 @@ import webpHtmlNosvg from 'gulp-webp-html-nosvg';
 import versionNumber from 'gulp-version-number';
 
 export const html = () => {
-  return app.gulp
-    .src(app.path.src.html) // get access to files
-    .pipe(
-      app.plugins.plumber(
-        app.plugins.notify.onError({
-          title: 'HTML',
-          message: 'Error: <%= error.message %>',
-        })
+  return (
+    app.gulp
+      .src(app.path.src.html) // get access to files
+      // error processing
+      .pipe(
+        app.plugins.plumber(
+          app.plugins.notify.onError({
+            title: 'HTML',
+            message: 'Error: <%= error.message %>',
+          })
+        )
       )
-    ) // error processing
-    .pipe(fileInclude()) // copy file with attached content
-    .pipe(app.plugins.replace(/@img\//g, 'img/')) // replace '@img' to correct path
-    .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg())) // conver images format to webp
-    .pipe(
-      app.plugins.if(
-        app.isBuild,
-        versionNumber({
-          value: '%DT%',
-          append: {
-            key: '_v',
-            cover: 0,
-            to: ['css', 'js'],
-          },
-          output: {
-            file: 'gulp/version.json',
-          },
-        })
+      .pipe(fileInclude()) // copy file with attached content
+      .pipe(app.plugins.replace(/@img\//g, 'img/')) // replace '@img' to correct path
+      .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg())) // conver images format to webp
+      // add version for cache
+      .pipe(
+        app.plugins.if(
+          app.isBuild,
+          versionNumber({
+            value: '%DT%',
+            append: {
+              key: '_v',
+              cover: 0,
+              to: ['css', 'js'],
+            },
+            output: {
+              file: 'gulp/version.json',
+            },
+          })
+        )
       )
-    ) // add version for cache
-    .pipe(app.gulp.dest(app.path.build.html))
-    .pipe(app.plugins.browsersync.stream());
+      .pipe(app.gulp.dest(app.path.build.html))
+      .pipe(app.plugins.browsersync.stream())
+  );
 };
