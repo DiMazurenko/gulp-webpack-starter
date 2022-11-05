e699bef032a296ed895afbece252ca81dcb44057
Dmitriy <dimazurenko@gmail.com>
2022-11-05 13:48:21 +0400

    feat: add images min function

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,7 +2,7 @@ import replace from 'gulp-replace'; // search and replace file
 import plumber from 'gulp-plumber'; // Error processing
 import notify from 'gulp-notify'; // notify (prompt)
 import browsersync from 'browser-sync'; // localhost
-import newer from 'gulp-newer'; // Проверка обновления
+import newer from 'gulp-newer'; // check update
 import ifPlugin from 'gulp-if'; // Условное ветвление
 
 export const plugins = {
